902ef0b0f8db902c2a52fdffe79c91c3c57c1eaa
Nico Suarez <ensu@splitmedialabs.com>
2021-08-04 16:18:31 +0800

    match-kernel not correctly working for arch kernel

--- bin/match-kernel.sh
+++ bin/match-kernel.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 RUNNING=$(uname -r)
-INSTALLED=$(pacman -Q linux | awk '{print $2}')
+INSTALLED=$(pacman -Q linux | awk '{print $2}' | sed 's/\.arch/-arch/')
 
-if [[ $RUNNING != $INSTALLED ]]; then
-    echo -ne "Needs Reboot"
-else
-    echo -ne $RUNNING;
+if [[ $RUNNING = $INSTALLED ]]; then
+    exit 0
 fi
 
-
+exit 1
